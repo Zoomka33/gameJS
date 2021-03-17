d91227990123db14b155dc738a835dbf148225af
Maxim Kalinin <maks.kalinin2000@mail.ru>
2021-03-17 14:12:24 +0300

    dino game 1.5v movment dirt and score system

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 const dino = document.querySelector('#dino');
 const cactus = document.querySelector('#cactus');
+const score = document.querySelector('.score');
+let scoreToken = 0;
 
 document.addEventListener("keydown", function (e){
-    jump();
+    if (e.code == 'ArrowUp' || e.code == 'Space'){
+        jump();
+        scoreToken = scoreToken+1;
+        score.innerHTML = scoreToken;
+        // moveRight();
+    }
 });
 
 function jump(){
@@ -13,11 +20,28 @@ function jump(){
         dino.classList.remove('jump')
     },300)
 }
+
+
+//Заготовка под движения влево вправо
+// function moveRight(){
+//     let left = parseInt(window.getComputedStyle(dino).getPropertyValue('left'));
+//     setInterval(function() {
+//             left = left + 1;
+//             dino.style.left = left + 'px';
+//         },10
+//     );
+// }
+
+
+
+
 let isAliive = setInterval(function (){
     let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue('top'));
     let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue('left'));
-
-    if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140){
-        alert('game over')
+    let left = parseInt(window.getComputedStyle(dino).getPropertyValue('left'));
+    if (cactusLeft < left+25 && cactusLeft > left-25 && dinoTop >= 140){
+        alert('Вы проиграли! Счет: ' + scoreToken);
+        dino.style.left = 0 + 'px';
+        scoreToken =0;
     }
 }, 10)
